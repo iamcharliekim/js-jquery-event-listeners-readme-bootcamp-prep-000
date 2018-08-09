7f9da7fa3941afcce279359341cf2c25d0bc3862
Charles Kim <cwkim3@gmail.com>
2018-08-09 21:00:44 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,23 @@
-//define functions here
+function getIt(){
+  $('p').on("click", function(){
+    alert("Hey!");
+  }
+  );
+}
+
+function frameIt(){
+  $('img').on("load", function(){
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('#typing').on("keydown", function(e){
+  if (e.which === 7){
+    alert ("You have pressed G");
+  }
+  });
+}
 
 $(document).ready(function(){
 
